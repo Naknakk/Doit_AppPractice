27b7c39f5df853f02d29b65f8706dfd480b513ae
YunHak Lee <lyh990408@gmail.com>
2023-07-18 18:21:40 +0900

    [✧MINER] 3-4 아웃렛 변수와 액션 함수 추가하기

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchingImageOnOff(_ sender: UISwitch) {
+    }
 }
 
