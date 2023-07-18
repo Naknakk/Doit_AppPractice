ad0c9dc4c7af471f82ca25b375436089983fd025
YunHak Lee <lyh990408@gmail.com>
2023-07-18 17:38:15 +0900

    [✧MINER] 실습용 프로젝트와 이미지 추기 (#6)

---
+++ ImageView/ImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by 이윤학 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
