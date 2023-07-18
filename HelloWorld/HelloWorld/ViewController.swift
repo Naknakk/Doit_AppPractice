3ce6b7908f50a6e46628497546feae3524690c4b
YunHak Lee <lyh990408@gmail.com>
2023-07-18 17:04:47 +0900

    [✧MINER] 시뮬레이션 실행 및 코드 주석 추가

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var lblHello: UILabel!
-    
-    @IBOutlet var txtName: UITextField!
+    @IBOutlet var lblHello: UILabel!        // 출력 레이블용 아웃렛 변수
+    @IBOutlet var txtName: UITextField!     // 이름 입력용 아웃렛 변수. textField에 입력한 내용으로 업데이트된다.
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +18,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnSend(_ sender: UIButton) {
+        // 출력 텍스트를 업데이트한다.
+        
         lblHello.text = "Hello, " + (txtName.text ?? "")
+        
     }
     
 }
